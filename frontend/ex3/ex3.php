228b9e1bb43eea7330e74d9729e6a90350bed120
GabrielGroppo <gabrielmgruppogg@gmail.com>
2023-04-25 10:56:51 -0300

    ex3

---
+++ frontend/ex3/ex3.php
@@ -0,0 +1,88 @@
+<?php
+// $_GET obtém dado do formulário enviado via GET
+// $_POST obtém dado do formulário enviado via POST
+
+$estado = $_POST['estado'];
+
+if ($estado == "AC"){
+    echo "Rio Branco";
+}
+if ($estado == "AL"){
+    echo "Maceió";
+}
+if ($estado == "AP"){
+    echo "Macapá";
+}
+if ($estado == "AM"){
+    echo "Manaus";
+}
+if ($estado == "CE"){
+    echo "Fortaleza";
+}
+if ($estado == "DF"){
+    echo "Brasilia";
+}
+if ($estado == "ES"){
+    echo "Vitória";
+}
+if ($estado == "GO"){
+    echo "Goiânia";
+}
+if ($estado == "MA"){
+    echo "São Luís";
+}
+if ($estado == "MT"){
+    echo "Cuiabá";
+}
+if ($estado == "MS"){
+    echo "Campo Grande";
+}
+if ($estado == "MG"){
+    echo "Belo Horizonte";
+}
+if ($estado == "PA"){
+    echo "Belém";
+}
+if ($estado == "PB"){
+    echo "João Pessoa";
+}
+if ($estado == "PR"){
+    echo "Curitiba";
+}
+if ($estado == "PE"){
+    echo "recife";
+}
+if ($estado == "PI"){
+    echo "Teresina";
+}
+if ($estado == "RJ"){
+    echo "Rio de Janeiro";
+}
+if ($estado == "RN"){
+    echo "Nata";
+}
+if ($estado == "RS"){
+    echo "Porto Alegre";
+}
+if ($estado == "RO"){
+    echo "Porto Velho";
+}
+if ($estado == "RR"){
+    echo "Boa Vista";
+}
+if ($estado == "SC"){
+    echo "Florianópolis";
+}
+if ($estado == "SP"){
+    echo "São Paulo";
+}
+if ($estado == "SE"){
+    echo "Aracaju";
+}
+if ($estado == "TO"){
+    echo "Palmas";
+}
+
+
+
+?>
